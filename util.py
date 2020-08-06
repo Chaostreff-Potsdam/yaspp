8783018a52b9adf230103c196278c2009a059d25
Matthias Jacob <dev@matthiasjacob.net>
2020-08-07 00:31:33 +0200

    add function to create chapter file from YAML

--- util.py
+++ util.py
@@ -15,6 +15,26 @@ INPUT = """00:00:00.000 Intro
 00:55:06.622 Abschluss mit Hotfix
 00:56:34.209 Musik: !Cube - Glittering Waves"""
 
+YAML_INPUT = """
+chapters:
+  - start: '00:00:00.000'
+    title: 'Intro'
+  - start: '00:00:49.000'
+    title: 'Musik: Professor Kliq - Bust This Bust That'
+    href: 'https://freemusicarchive.org/music/Professor_Kliq/Bust_This_Bust_That/Bust_This_Bust_That'
+  - start: '00:05:03.000'
+    title: 'Aktuelles vom Chaostreff'
+  - start: '00:09:44.000'
+    title: 'Corona-Warn-App'
+  - start: '00:28:40.700'
+    title: 'Musik: Organism - Space Funk'
+    href: 'https://freemusicarchive.org/music/Organism/S27-X_II/Organism_-S27-X_II-_19_Space_Funk'
+  - start: '00:33:59.000'
+    title: 'Kurzmeldungen'
+  - start: '00:58:30.000'
+    title: 'Erdbeerschnitzel - Walkampfchampagne'
+    href: 'https://freemusicarchive.org/music/Erdbeerschnitzel/Pathetik_Party/02-Walkampfchampagne'
+"""
 
 def generate_yaml(content):
     lines = content.splitlines()
@@ -25,7 +45,19 @@ def generate_yaml(content):
 
         print(template)
 
+def generate_chapter_file():
+    import yaml
+
+    content = yaml.safe_load(YAML_INPUT)
+
+    for chapter in content['chapters']:
+        template = f"{chapter['start']} {chapter['title']}"
+
+        print(template)
+
+    #read_content_yaml()
 
 if __name__ == '__main__':
-    generate_yaml(INPUT)
+    #generate_yaml(INPUT)
+    generate_chapter_file()
 
